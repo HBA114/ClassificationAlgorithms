03b2817fc58b50d7024a6588296f7093a2ccf2ee
HBA114 <hbasriayhaner114@gmail.com>
2022-11-01 00:33:57 +0300

    Updates On NaiveBayes and Program files

--- ConsoleApp/NaiveBayes/NaiveBayes.cs
+++ ConsoleApp/NaiveBayes/NaiveBayes.cs
@@ -1,5 +1,59 @@
 namespace ConsoleApp.NaiveBayes;
 public class NaiveBayes
 {
-    // TODO: Verilerin Tamamı Sayısal Bu Yüzden Standart Sapma, Ortalama, Karekök ve üstel sayılar kullanılacaktır.
+    public NaiveBayes()
+    {
+        
+    }
+
+    public void TrainNaiveBayesModel(List<string> trainData)
+    {
+        // her sınıf için ortalama ve standart sapma değerleri hesaplanır.
+    }
+
+    public void TestNaiveBayes(List<string> testDataSet)
+    {
+        // her sınıf için hesaplanan veriler kullanılarak test verisinin sınıfı tahmin edilmeli.
+        // her tahmin doğruluğu veya yanlışlığı belirlenerek listelenmeli ve sonuç olarak doğruluk ölçüsü Accuracy gösterilmeli.
+    }
+
+    public double Mean(double[] values)
+    {
+        double sum = 0;
+        foreach (double number in values)
+        {
+            sum += number;
+        }
+
+        return sum / values.Count();
+    }
+
+    public double StandartDeviation(double[] values)
+    {
+        double sum = 0;
+        double mean = Mean(values);
+        int count = values.Count() - 1;
+
+        foreach (double number in values)
+        {
+            sum += Math.Pow((number - mean), 2);
+        }
+        sum /= count;
+
+        return Math.Sqrt(sum);
+    }
+
+    public void CalculateYesNoSample(double temp)
+    {
+        double yesMean = Mean(new double[] { 27, 16, 5 });
+        double noMean = Mean(new double[] { 25, 30, 8 });
+        double yesStandartDeviation = StandartDeviation(new double[] { 27, 16, 5 });
+        double noStandartDeviation = StandartDeviation(new double[] { 25, 30, 8 });
+
+        double yes = 1 / (yesStandartDeviation * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -1 / 2 * Math.Pow((temp - yesMean / yesStandartDeviation), 2));
+        double no = 1 / (noStandartDeviation * Math.Sqrt(2 * Math.PI)) * Math.Pow(Math.E, -1 / 2 * Math.Pow((temp - noMean / noStandartDeviation), 2));
+
+        System.Console.WriteLine(yes / (yes + no));
+        System.Console.WriteLine(no / (yes + no));
+    }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApp.Data;
+using ConsoleApp.NaiveBayes;
 
 string filePath = "ConsoleApp/Data/dry_bean_dataset.csv";
 string trainDataPath = "ConsoleApp/Data/dry_bean_dataset_train.csv";
@@ -9,11 +10,24 @@ DataProcessing dataProcessing = new DataProcessing(filePath);
 // if you do not want to save train and test data:
 // Use SeperateTrainAndTest method with only 1 argument trainDataPercentile
 
-Tuple<List<string>, List<string>> seperatedData = await dataProcessing.SeperateTrainAndTest(0.7f, trainDataPath, testDataPath);
-List<string> trainData = seperatedData.Item1;
-List<string> testData = seperatedData.Item2;
+// Tuple<List<string>, List<string>> seperatedData = await dataProcessing.SeperateTrainAndTest(0.7f, trainDataPath, testDataPath);
+// List<string> trainData = seperatedData.Item1;
+// List<string> testData = seperatedData.Item2;
 
-System.Console.WriteLine("Train Data Count : " + trainData.Count());
-System.Console.WriteLine("Test Data Count : " + testData.Count());
+// System.Console.WriteLine("Train Data Count : " + trainData.Count());
+// System.Console.WriteLine("Test Data Count : " + testData.Count());
 
 // TODO: Create NaiveBayes and KNN Algorithms And Test Them
+
+NaiveBayes naiveBayes = new NaiveBayes();
+
+System.Console.WriteLine(naiveBayes.Mean(new double[] { 27, 16, 5 }));
+System.Console.WriteLine(naiveBayes.Mean(new double[] { 25, 30, 8 }));
+
+System.Console.WriteLine(naiveBayes.StandartDeviation(new double[] { 27, 16, 5 }));
+System.Console.WriteLine(naiveBayes.StandartDeviation(new double[] { 25, 30, 8 }));
+
+// naiveBayes.CalculateYesNoSample(23);
+
+// naiveBayes.TrainNaiveBayesModel(trainData);
+// naiveBayes.TestNaiveBayes(testData);
