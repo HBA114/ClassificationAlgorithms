088a3e485cf825e988d4b405d29d1a64924bb40f
Hasan Basri Ayhaner <hasan.ayhaner@nextlua.com>
2023-08-08 15:04:35 +0300

    Added Neighbor entity to Entity project

---
+++ Entities/Neighbor.cs
@@ -0,0 +1,13 @@
+namespace Entities;
+
+public class Neighbor
+{
+    public string Name;
+    public double Value;
+
+    public Neighbor(string name, double value)
+    {
+        Name = name;
+        Value = value;
+    }
+}
